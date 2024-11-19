682accdf527fe84dc74d1826fdbc6ff4d379e5cc
Asus <Asus@JUAN-01>
2024-11-19 17:15:16 -0500

    Prueba

---
+++ Documents/NetBeansProjects/TareaSemana4/src/tareasemana4/TareaSemana4.java
@@ -0,0 +1,13 @@
+package tareasemana4;
+// Tarea Semana 4
+// @author Juan Ángel Leguizamón
+ 
+public class TareaSemana4 {
+    public static void main(String[] args) {
+
+        //9 imprimir los numeros del 1 al 9
+        for (int c = 1; c<=9; c++) {   // Ejecutamos la operacion en un for
+        System.out.println(c); // El operador ++ no se ejecuta dentro del print
+        }
+    }
+}
